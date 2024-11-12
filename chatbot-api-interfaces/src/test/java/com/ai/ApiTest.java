654a41aa304b98b1a8123c22bced3a5ce707f66d
sour <2387988401@qq.com>
2024-11-12 11:13:11 +0800

    sour initProject

---
+++ chatbot-api-interfaces/src/test/java/com/ai/ApiTest.java
@@ -0,0 +1,13 @@
+package com.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTest {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ApiTest.class,args);
+    }
+}
